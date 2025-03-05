516f73ae22fb0d7835648a0c623a1a80a5cef0ba
mdrasel202 <raselabc204@gmail.com>
2025-03-05 13:41:21 +0600

    done

--- first-project/src/main/java/com/rasel/first_project/controller/StudentController.java
+++ first-project/src/main/java/com/rasel/first_project/controller/StudentController.java
@@ -1,5 +1,43 @@
 package com.rasel.first_project.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rasel.first_project.model.Student;
+import com.rasel.first_project.service.StudentService;
+
+@RestController
+@RequestMapping(value = "/student")
 public class StudentController {
 
+	private final StudentService service;
+
+	public StudentController(StudentService service) {
+		this.service = service;
+	}
+
+	@PostMapping
+	public Student saveStudent(@RequestBody Student student) {
+		Student saveStudent = service.saveStudent(student);
+
+		return saveStudent;
+	}
+
+	@GetMapping
+	public List<Student> getStudents() {
+
+		return service.getStudents();
+	}
+
+	@DeleteMapping("/{id}")
+	public void deleteById(@PathVariable int id) {
+		service.deleteById(id);
+	}
 }
--- first-project/src/main/java/com/rasel/first_project/service/StudentService.java
+++ first-project/src/main/java/com/rasel/first_project/service/StudentService.java
@@ -1,5 +1,32 @@
 package com.rasel.first_project.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.rasel.first_project.model.Student;
+import com.rasel.first_project.repository.StudentRepository;
+
+@Service
 public class StudentService {
 
+	private final StudentRepository repository;
+
+	public StudentService(StudentRepository repository) {
+		this.repository = repository;
+	}
+
+	public Student saveStudent(Student student) {
+		Student saved = repository.save(student);
+		return saved;
+	}
+
+	public List<Student> getStudents() {
+		// TODO Auto-generated method stub
+		return repository.findAll();
+	}
+
+	public void deleteById(int id) {
+		repository.deleteById(id);
+	}
 }
